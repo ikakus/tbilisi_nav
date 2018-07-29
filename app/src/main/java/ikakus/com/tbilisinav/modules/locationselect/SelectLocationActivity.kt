359e7343d66f4d82670f51724ca1cb0a15a5ab30
ikakus <idadianidev@gmail.com>
2018-07-29 18:55:24 +0400

    Icons added. Color and strings fix

--- app/src/main/java/ikakus/com/tbilisinav/modules/locationselect/SelectLocationActivity.kt
+++ app/src/main/java/ikakus/com/tbilisinav/modules/locationselect/SelectLocationActivity.kt
@@ -76,10 +76,12 @@ class SelectLocationActivity : BaseActivity(), MviView<SelectLocationIntent, Sel
     override fun render(state: SelectLocationViewState) {
         isStart = true
         if (state.startLocation == null) {
+            imageView.setImageDrawable(resources.getDrawable(R.drawable.ic_pin_a))
             tvFrom.text = getString(R.string.not_selected)
             buttonSelectLocation.text = getString(R.string.select_start)
 
         } else {
+            imageView.setImageDrawable(resources.getDrawable(R.drawable.ic_pin_b))
             tvFrom.text = state.startLocation.toString()
             startLocation = state.startLocation
             buttonSelectLocation.text = getString(R.string.select_end)
